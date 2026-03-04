50c2943600b25b504d6c1ef7c1baee59e2151b6b
Eshaniiii2004 <eshanighanwat2004@gmail.com>
2026-03-04 17:20:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let statusElement=document.getElementById("status");
+let button=document.getElementById("enterBtn");
+button.addEventListener("click",function()
+						{
+							statusElement.innerHTML="<h1>Entered Metaverse</h1>;
+								
+						});
